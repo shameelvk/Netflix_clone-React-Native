94f07b36a837032004858cf4807c8670fe742824
shameelvk <shameel3311@gmail.com>
2024-06-11 12:02:42 +0530

    completed splash screen

--- src/screens/Splash/Splash.jsx
+++ src/screens/Splash/Splash.jsx
@@ -1,15 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { myColor } from '../../utils/Theme'
 
-const Splash = () => {
-    
+const Splash = ({navigation}) => {
+    useEffect(()=>{
+        setTimeout(() => {
+           navigation.replace('Home')
+            
+        }, 3000);
+    },[])
+
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={'transparent'} hidden/>
+      <Image style={styles.splashImg} source={require('../../assets/images/splashimg.png')}/>
     </View>
   )
 }
 
 export default Splash
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:myColor.primary,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    splashImg:{
+        width:"70%",
+        height:100
+    }
+
+})
